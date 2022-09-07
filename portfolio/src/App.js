056a13872fef8ee9dea66b05a26641100d17a2d3
mayragann <mayragann@gmail.com>
2022-09-07 09:45:14 -0400

    Added React Icon Package + Added Functionality to Sidebar to expand or toggle

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Home from './Component/Home/Home'
+
 import Sidebar from './Component/Sidebar/Sidebar'
 
 const App = () => {
   return (
     <div>
       <Sidebar />
-      <Home />
+ 
     </div>
   )
 }
--- portfolio/src/Component/Sidebar/Sidebar.jsx
+++ portfolio/src/Component/Sidebar/Sidebar.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Sidebar.css"
+import Home from '../Home/Home'
+import {BsChevronBarRight, BsChevronBarLeft} from "react-icons/bs";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+    const [expandSidebar, setExpandSidebar] = useState(true);
+    const handleExpandClick = () => { // if clicked becomes opposite of state
+        setExpandSidebar(!expandSidebar)
+    }
+    return (
+        <div className='container-fluid sidebar-section'>
+            {/* this is toggle for state, if not one state will be other */}
+            <div className={
+                expandSidebar ? 'sidebar-expand sidebar' : 'sidebar'
+            }>
+                <div className='icon-for-sidebar-expand-and-collapse'>
+                    <p onClick={handleExpandClick}>
+                        {
+                        expandSidebar ? (
+                            <BsChevronBarLeft size={30}/>
+                        ) : (
+                            <BsChevronBarRight size={30}/>
+                        )
+                    } </p>
+
+                </div>
+            </div>
+
+
+            <div className='container'>
+                <Home/>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Sidebar
