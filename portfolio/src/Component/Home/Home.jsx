cf42e1570a95dc28d2a0f7a11e9051e8680eb24a
mayragann <mayragann@gmail.com>
2022-09-07 11:17:47 -0400

    Added Buttons for Contact + Fully functional Download Resume Button + Stylized both buttons

--- portfolio/src/Component/Home/Home.jsx
+++ portfolio/src/Component/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Home.css"
 import Typewriter from 'typewriter-effect';
+import resume from "./Mayra_Gann_resume.pdf"
 
 const Home = () => {
     return (
@@ -8,18 +9,29 @@ const Home = () => {
             <div className='container home-content'>
                 <h1>Hello I'm a</h1>
                 <h3>
-                <Typewriter options={
+                    <Typewriter options={
                         {
                             strings: [
                                 'Full Stack Software Developer', 'Web Developer', 'UI/UX Designer'
                             ],
                             autoStart: true,
                             loop: true,
-                            delay:5
+                            delay: 5
                         }
                     }/>
 
                 </h3>
+
+                <div className='button-for-action'>
+
+                    <div className='contact-button'>Contact</div>
+                    <div className='resume-button'>
+                        <a href={resume} download='Mayra_Gann_Resume.pdf'>
+                        Resume
+                        </a>
+                        </div>
+
+                </div>
             </div>
 
         </div>
